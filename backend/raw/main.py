86286f32d4b5cdc606a2198c788eb086eeb9dfa8
HitKoladiya <104366639+HitKoladiya@users.noreply.github.com>
2022-07-31 00:57:07 +0530

    all api file
    in this main file we call all function to generate api of naac,nirf.

---
+++ backend/raw/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from list_to_dic import table
+import requests
+from bs4 import BeautifulSoup as b
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/api')
+def api():
+    url = "https://www.nirfindia.org/2022/OverallRanking.html"
+
+    data=table(url)
+    # print(data)
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#http://127.0.0.1:5000
+
