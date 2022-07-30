49f792cc26f0da71433ffaab6064bbdaf38112e1
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2022-07-30 22:21:41 +0530

    colleges and their naac rank dictionary

---
+++ backend/raw/exl.py
@@ -0,0 +1,33 @@
+from openpyxl import load_workbook
+file_path = "Institutions_accredited_by_NAAC_whose_accreditation_period_is_valid_08_07_22.xlsx"
+wb = load_workbook(file_path)
+ws = wb["Universities"]
+
+
+coloum = ws['C']
+data = []
+for  x in range(len(coloum)):
+    a = coloum[x].value
+    data.append(a)
+
+
+coloum4 = ws['F']
+data1 = []
+for  y in range(len(coloum4)):
+    b = coloum4[y].value
+    data1.append(b)
+
+
+for x in range(6):
+    del data[0]
+    del  data1[0]
+
+d = dict(zip(data,data1))
+print(d)
+
+with open("naac.txt", "w") as external_file:
+    print(data, file=external_file)
+    external_file.close()
+# for item in d:
+#     if item == 'Rashtriya Sanskrit Vidyapeetha':
+#         print(d[item])
