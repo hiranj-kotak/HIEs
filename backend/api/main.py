31a5230d20549349f3890ef63629a8f39c8ddb11
Hiranj kotak <21it068@charusat.eu.in>
2022-07-31 09:24:33 +0530

    moved main.py to api folder

---
+++ backend/api/main.py
@@ -0,0 +1,31 @@
+from flask import Flask,jsonify
+from flask_cors import CORS, cross_origin
+from backend.raw.nirf import nirf_final
+from backend.raw.naac_exl import naac
+from bs4 import BeautifulSoup as b
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/foo": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/api1')
+def api1():
+    data1 = nirf_final()
+    return jsonify(data1)
+
+@app.route('/api2')
+def api2():
+    data2=naac()
+    # print(data2)
+    return jsonify(data2)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#http://127.0.0.1:5000
+
