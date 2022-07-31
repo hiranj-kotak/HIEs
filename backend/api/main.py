6235f26ad3a4b8959b1fde4147c8f28c4a3edaaf
HitKoladiya <104366639+HitKoladiya@users.noreply.github.com>
2022-07-31 15:11:19 +0530

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,21 +1,25 @@
-from flask import Flask,jsonify
-from flask_cors import CORS, cross_origin
-from backend.raw.nirf import nirf_final
-from backend.raw.naac_exl import naac
-from bs4 import BeautifulSoup as b
+from flask import Flask, jsonify
+from nirf import nirf
+from naac_exl import naac
+from flask_cors import CORS
+# import requests
+# from bs4 import BeautifulSoup as b
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/foo": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/api1')
+@app.route('/api1/', methods=['GET'])
 def api1():
-    data1 = nirf_final()
+    data1=nirf()
+    # data1.headers.add('Access-Control-Allow-Origin', '*')
+    print(data1)
     return jsonify(data1)
+
 
 @app.route('/api2')
 def api2():
@@ -24,8 +28,7 @@ def api2():
     return jsonify(data2)
 
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-#http://127.0.0.1:5000
 
