dcf8ca372c4898b26759fa9cc97b41b03f0871a9
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-08-02 19:07:16 +0530

    login form handled

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import validator from "email-validator";
+//
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -6,6 +11,57 @@ import Card from "react-bootstrap/Card";
 import loginImg from "../assets/loginPencil.jpg";
 
 const Login = () => {
+    const MySwal = withReactContent(Swal);
+
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
+
+    let name, value;
+
+    const handleInputs = (e) => {
+        // console.log(e);
+        name = e.target.name;
+        value = e.target.value;
+
+        setUser({ ...user, [name]: value });
+    };
+
+    const postData = async (e) => {
+        e.preventDefault();
+
+        const { email, password } = user;
+
+        if (!email || !password) {
+            MySwal.fire({
+                icon: "error",
+                title: <p>Error!</p>,
+                text: "All details are required",
+            });
+        } else if (validator.validate(email) == false) {
+            MySwal.fire({
+                icon: "error",
+                title: <p>Error!</p>,
+                text: "Please enter valid email",
+            });
+        } else {
+            console.log(user);
+
+            MySwal.fire({
+                icon: "success",
+                title: <p>login successfull</p>,
+                text: `Welcome back ${email}`,
+            });
+
+            setUser({ email: "", password: "" });
+
+            // Send user to backend is remaining
+            navigate("/");
+        }
+    };
     return (
         <>
             <Container className="my-5 d-flex justify-content-center">
@@ -22,6 +78,10 @@ const Login = () => {
                                 <Form.Control
                                     type="email"
                                     placeholder="Enter email"
+                                    name="email"
+                                    autoComplete="off"
+                                    value={user.email}
+                                    onChange={handleInputs}
                                 />
                                 <Form.Text className="text-muted">
                                     We'll never share your email with anyone
@@ -36,9 +96,17 @@ const Login = () => {
                                 <Form.Control
                                     type="password"
                                     placeholder="Password"
+                                    name="password"
+                                    autoComplete="off"
+                                    value={user.password}
+                                    onChange={handleInputs}
                                 />
                             </Form.Group>
-                            <Button variant="primary" type="submit">
+                            <Button
+                                variant="primary"
+                                type="submit"
+                                onClick={postData}
+                            >
                                 Login
                             </Button>
                         </Form>
