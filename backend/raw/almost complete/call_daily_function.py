3cf8465f371931abe7dbc0723af82ab96c71d2e0
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2022-08-07 03:31:50 +0530

    convderted text file to json file

--- backend/raw/almost complete/call_daily_function.py
+++ backend/raw/almost complete/call_daily_function.py
@@ -1,25 +1,26 @@
 from naac_exl import naac
 from nirf100 import nirf
 from update_naac_and_nirf_database import nirf_database,naac_database
+import  json
 import time
 
 def daily_update():
     def func():
         naac_data=naac()
-        with open("../naac.txt", "w") as external_file:
-            print(naac_data, file=external_file)
+        with open("../naac.json", "w") as external_file:
+            json.dump(naac_data, external_file)
             external_file.close()
         naac_database(naac_data)
 
         nirf_data=nirf()
-        with open("../nirf.txt", "w") as external_file:
-            print(nirf_data, file=external_file)
+        with open("../nirf.json", "w") as external_file:
+            json.dump(nirf_data,external_file)
             external_file.close()
         nirf_database(nirf_data)
 
-
-    # schedule.every().day.at("02:00").do(func)
-    # schedule.every().minutes.do(func)
+    # func()
+    schedule.every().day.at("02:00").do(func)
+    schedule.every().minutes.do(func)
 
     while True:
         func()
