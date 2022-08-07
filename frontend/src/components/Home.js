aa727d56b002551e749d3bbd8be144c80e8978af
Jay A. Keraliya <jaykeraliya0@gmail.com>
2022-08-07 17:42:41 +0530

    changed home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,23 +1,24 @@
 import React from "react";
+import Showrank from "./Showrank";
 
 const Home = () => {
     return (
         <>
-            <div class="position-relative overflow-hidden p-0 p-md-5 text-center semi-bg text-bg-dark">
+            <div class="position-relative overflow-hidden p-0 p-md-5 text-center semi-bg text-success">
                 <div class="col-md-5 p-lg-5 mx-auto my-5">
-                    <h1 class="display-4 fw-normal">Title</h1>
+                    {/* <h1 class="display-4 fw-normal">Title</h1>
                     <p class="lead fw-normal">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Eum porro quis minus natus voluptatibus blanditiis
                         inventore consequuntur magnam aut, a aliquam in
                         voluptatum ea, voluptates corrupti. Tempora sint eaque
                         eum.
-                    </p>
+                    </p> */}
                 </div>
                 <div class="product-device shadow-sm d-none d-md-block"></div>
                 <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
             </div>
-            <div
+            {/* <div
                 class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3"
                 style={{ width: "100vw" }}
             >
@@ -49,7 +50,8 @@ const Home = () => {
                         }}
                     ></div>
                 </div>
-            </div>
+            </div> */}
+            <Showrank />
         </>
     );
 };
