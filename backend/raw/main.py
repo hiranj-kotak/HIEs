d113c270082e47c7a370ae47d701f65358039aea
HitKoladiya <koladiyahit45@gmail.com>
2022-08-08 13:34:28 +0530

    update

--- backend/raw/main.py
+++ backend/raw/main.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, request
-from nirf100 import  nirf
+# from nirf100 import  nirf
 from flask_cors import CORS
 # from database import nirf_api
+from naac_data_sort import naac_sort
+from college_database import college_data_entry
 
 app = Flask(__name__)
 
 CORS(app, resources={r"/nirf/": {"origins": "*"}})
 CORS(app, resources={r"/college/": {"origins": "*"}})
-# CORS(app, resources={r"/naac1/": {"origins": "*"}})
+CORS(app, resources={r"/user/": {"origins": "*"}})
 # CORS(app, resources={r"/naac2/": {"origins": "*"}})
 # CORS(app, resources={r"/naac3/": {"origins": "*"}})
 
@@ -18,14 +20,14 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/nirf/', methods=['GET'])
-def nirf1():
-    data100 = nirf()
-
-    # data150 = nirf150()
-    # data1.headers.add('Access-Control-Allow-Origin', '*')
-    # print(data1)
-    return jsonify(data100)
+# @app.route('/naac/', methods=['GET'])
+# def nirf1():
+#     data100 = naac_sort()
+#
+#     # data150 = nirf150()
+#     # data1.headers.add('Access-Control-Allow-Origin', '*')
+#     # print(data1)
+#     return jsonify(data100)
 
 
 
@@ -33,8 +35,8 @@ def nirf1():
 def get_detail_of_college():
     content=request.get_json()
     # print(content)
-    data0=validate_nirf(content)
-    data00=validate_naac(content)
+    # data0=validate_nirf(content)
+    # data00=validate_naac(content)
     # x=(str(data0["NIRFrank"]))
     # print(data00)
     data1={
@@ -44,6 +46,24 @@ def get_detail_of_college():
         "type" : content['type']
     }
     return data1
+
+@app.route('/user/' ,methods=['POST'])
+def put_detail_of_college():
+    return "hii"
+
+
+@app.route('/college_data/', methods=['POST'])
+def college_data():
+    content = request.get_json()
+    data=college_data_entry(content)
+    print(data)
+    return data
+#
+#
+# @app.route('/user_signup/', methods=['POST'])
+# def put_detail_of_college():
+#     return "hii"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
